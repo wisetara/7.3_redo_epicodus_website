fe6933a5a48c79011018fa250f9b02d686a03c44
Tara <taratirzah@gmail.com>
2014-04-02 16:57:37 -0700

    End of day commit - re-organization needed

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ LearnHowToProgram::Application.routes.draw do
   match('lessons', {:via => :get, :to => 'lessons#index'})
   match('lessons', {:via => :post, :to => 'lessons#create'})
   match('lessons/new', {:via => :get, :to => 'lessons#new'})
-  match('lessons/:id', {:via => [:patch, :put], :to => 'lessons#update'})
   match('lessons/:id', {:via => :get, :to => 'lessons#show'})
   match('lessons/:id/edit', {:via => :get, :to => 'lessons#edit'})
+  match('lessons/:id', {:via => [:patch, :put], :to => 'lessons#update'})
   match('lessons/:id', {:via => :delete, :to => 'lessons#destroy'})
   match('/', {:via => :get, :to => 'lessons#index'})
 end
